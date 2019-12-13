216e381b1bde30a6c6fee940d3c918b75e1a083f
Jules Lee <jackconsolidated@gmail.com>
2019-12-13 18:33:39 +0800

    Initial Commit

---
+++ InstagramApp/ViewControllers/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  InstagramApp
+//
+//  Created by Jules Lee on 13/12/2019.
+//  Copyright © 2019 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
